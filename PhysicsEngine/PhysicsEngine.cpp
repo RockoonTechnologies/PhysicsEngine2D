bb1a92f7a2087b2000504fbc4dc723697acc13ac
RockoonTechnologies <66290320+RockoonTechnologies@users.noreply.github.com>
2021-02-08 12:29:30 -0500

    eeeeeeeeeee

--- PhysicsEngine/PhysicsEngine.cpp
+++ PhysicsEngine/PhysicsEngine.cpp
@@ -2,9 +2,20 @@
 //
 
 #include <iostream>
+#include <string>
 #include "Rigidbody.h"
+
+using namespace std;
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    Rigidbody rb = Rigidbody(3, Vector2(0, 0));
+    
+    for (int x = 0; x < 20; x++) {
+        rb.Update();
+        cout << to_string(rb.position.x) + "," + to_string(rb.position.y) + "\n";
+    }
 }
 
